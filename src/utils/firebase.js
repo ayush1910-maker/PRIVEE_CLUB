f87d6e9e0a1ba569025c535062a941295fe19c3b
Ayush Porwal <pa9067628@gmail.com>
2025-12-29 18:00:33 +0530

    intialize firebase.js

---
+++ src/utils/firebase.js
@@ -0,0 +1,6 @@
+import admin from "firebase-admin";
+import serviceAccount from "./firebase-service-account.json";
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
