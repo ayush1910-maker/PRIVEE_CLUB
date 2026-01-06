746787a857155cc50e7643c55a3b843d2475abed
Ayush Porwal <pa9067628@gmail.com>
2026-01-06 17:46:45 +0530

    implement 1-1 chat with socket.io

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,5 +1,6 @@
 import { Server } from "socket.io";
 import Messages from "../models/messages.model.js";
+import { Op, Sequelize } from "sequelize";
 
 let io; 
 const userSocketMap = new Map(); 
@@ -21,12 +22,8 @@ export const initSocket = (server) => {
 
     socket.on("join_chat" , async ({sender_id , receiver_id}) => {
         try {
-        const roomId =
-          Number(sender_id) < Number(receiver_id)
-            ? `${sender_id}_${receiver_id}`
-            : `${receiver_id}_${sender_id}`;
 
-        socket.join(roomId);
+        console.log("join_chat event received", sender_id, receiver_id);
 
         const messages = await Messages.findAll({
           where: {
@@ -35,7 +32,7 @@ export const initSocket = (server) => {
               { sender_id: receiver_id, receiver_id: sender_id },
             ],
           },
-          order: [["createdAt", "ASC"]],
+          order: [["created_at", "ASC"]],
         });
 
         socket.emit("chat_history", messages);
@@ -57,16 +54,46 @@ export const initSocket = (server) => {
         }
             
         } catch (error) {
+          console.log(error);
+          
             socket.emit("message_error", {
                 error: "Message sending failed",
             });
         }
     })
-    
-    userSocketMap.set(String(userId), socket.id);
-    
-    io.emit("getOnlineUsers", Array.from(userSocketMap.keys()));
 
+    socket.on("fetch_senders" , async ({receiver_id}) => {
+      try {
+
+        const senders = await Messages.findAll({
+          attributes: [
+            [Sequelize.fn("DISTINCT" , Sequelize.col("sender_id")) , "sender_id"]
+          ],
+          where: { receiver_id },
+        })
+
+        const SenderIds = senders.map((s) => s.sender_id)
+
+        const SenderStatus = SenderIds.map((id) => ({
+          userId: id,
+          online: userSocketMap.has(String(id))
+        }))
+
+        socket.emit("sender_list" , SenderStatus)
+        
+      } catch (error) {
+        socket.emit("message_error" , {
+          error: "Message sending failed"
+        });
+      }
+    })
+    
+    userSocketMap.set(String(userId), socket.id);    
+    io.emit("getOnlineUsers", Array.from(userSocketMap.keys()));
+    console.log("User Socket Map:", userSocketMap);
+
+
+    // send message events
     socket.on("send_message", async ({ sender_id, receiver_id, content }) => {
         try {
             const message = await Messages.create({
@@ -76,23 +103,34 @@ export const initSocket = (server) => {
                 is_read: false,
             });
 
-            socket.emit("message_received", {
+            const payload =  {
                 id: message.id,
                 sender_id,
                 receiver_id,
                 content,
                 created_at: message.created_at,
-            });
+            }
+
+            const receiverSocketId = userSocketMap.get(String(receiver_id))
+            console.log("Receiver Socket ID:", receiverSocketId);
+
+            if (receiverSocketId) {
+              io.to(receiverSocketId).emit("message_received", payload);
+            }
+
+            // io.emit("message_received" , payload)
 
         } catch (error) {
-            console.error("Message send error:", error);
-            socket.emit("message_error", {
+          console.error("Message send error:", error);
+
+          socket.emit("message_error", {
             error: "Message sending failed",
-        });
+          });
     }
     });
 
 
+    // typing event 
     socket.on("typing", ({ senderId, receiverId }) => {
       const receiverSocketId = userSocketMap.get(String(receiverId));
       if (receiverSocketId) {
@@ -100,11 +138,13 @@ export const initSocket = (server) => {
       }
     });
 
+
+    //  disconnect event
     socket.on("disconnect", () => {
       userSocketMap.delete(String(userId));
-
       io.emit("getOnlineUsers", Array.from(userSocketMap.keys()));
     });
+
   });
 
   return io;
